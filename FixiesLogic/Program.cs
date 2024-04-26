a24a959bc71d01260d42d25e9d40eef5f5ec4678
caio.cunha <dev09@nodetech.com.br>
2024-04-25 21:06:32 -0300

    Conditional

--- FixiesLogic/Program.cs
+++ FixiesLogic/Program.cs
@@ -1,31 +1,21 @@
 ﻿using System;
 using System.Globalization;
 
-class Program 
+class Program
 
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("entre com seu nome completo");
-        string fullName = Console.ReadLine();
+        ///condicionais pt.1
+        Console.WriteLine("Qual a hora atual?");
 
-        int bedrooms = int.Parse(Console.ReadLine());
-        Console.WriteLine("Entre com seu preço de produto");
-
-        double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-        Console.WriteLine("Entre seu último nome, idade e altura (na mesma linha)");
-
-        string[] vect = Console.ReadLine().Split(' ');
-        string lastName = vect[0];
-        int age = int.Parse(vect[1]);
-        double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
-
-        Console.WriteLine(fullName);
-        Console.WriteLine(bedrooms);
-        Console.WriteLine(price.ToString("F2",CultureInfo.InvariantCulture));
-        Console.WriteLine(lastName);
-        Console.WriteLine(age);
-        Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        int hora = int.Parse(Console.ReadLine());
+        if (hora < 12)
+            Console.WriteLine("Bom dia!");
+        else if (hora < 18)
+            Console.WriteLine("Boa tarde!");
+        else { Console.WriteLine("Boa noite !"); }
 
     }
+
 }
